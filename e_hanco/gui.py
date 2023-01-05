b02dacbe439a3da09f8767e6ba2ecc843794ac4f
myurarin <matu.urari@gmail.com>
2023-01-06 00:37:02 +0900

    全体的に処理の見直しと保存の実装

--- e_hanco/gui.py
+++ e_hanco/gui.py
@@ -2,8 +2,13 @@ import os
 import sys
 import platform
 import tkinter as tk
+from tkinter import filedialog
 from PIL import Image, ImageDraw, ImageFont, ImageTk
-import time
+from datetime import datetime
+
+from e_hanco import hanco
+
+DT_NOW = datetime.now()
 
 
 class hanco_gui:
@@ -18,10 +23,10 @@ class hanco_gui:
     img_size: tuple = (300, 300)
     # ハンコ画像
     hanco_img = Image.new('RGBA', img_size, color_white)
-    # 各Box内のテキスト値
-    tk_company_box_txt: str = ""     # 社名テキスト
-    tk_date_box_txt: str = ""        # 日付テキスト
-    tk_name_box_txt: str = ""        # 名前テキスト
+    # 各Box
+    tk_company_box = None     # 社名テキスト
+    tk_date_box = None        # 日付テキスト
+    tk_name_box = None        # 名前テキスト
     # GUI表示画像
     canvas = None
     # tkinter root
@@ -36,3 +41,71 @@ class hanco_gui:
         else:
             # 対応するフォントが見つからないので
             raise FileNotFoundError("対応するフォントが見つかりません")
+
+        # 判子を初期化
+        self.hanco_img = hanco.drawing(self.font_type)
+
+    def hanco_thumbnail_update(self):
+        thumbnail = self.hanco_img.resize((150, 150))
+        thumbnail = ImageTk.PhotoImage(thumbnail)
+        self.canvas = tk.Canvas(bg="white", width=150, height=150)
+        self.canvas.place(x=0, y=0)
+        self.canvas.create_image(0, 0, image=thumbnail, anchor=tk.NW)
+        self.canvas.update()
+
+        self.root.mainloop()
+
+    def hanco_save(self):
+        save_file_path = filedialog.asksaveasfile(
+            title="名前を付けて判子を保存",
+            filetypes = [("PNG", ".png") ],
+            initialdir = "./",
+            defaultextension = "png"
+        )
+        self.hanco_img.save(save_file_path.name)
+
+    def button_event(self, event):
+        command = str(event.widget["text"])
+        if command == "update":
+            self.hanco_img = hanco.drawing(
+                self.font_type,
+                company_name=self.tk_company_box.get(),
+                seal_date=self.tk_date_box.get(),
+                seal_name=self.tk_name_box.get())
+            self.hanco_thumbnail_update()
+        if command == "save":
+            self.hanco_save()
+
+    def tk_init(self):
+        self.root.title(u"eHanco")
+        self.root.geometry("400x200")
+        self.root.resizable(width=False, height=False)
+
+        thumbnail = self.hanco_img.resize((150, 150))
+        thumbnail = ImageTk.PhotoImage(thumbnail)
+        self.canvas = tk.Canvas(bg="white", width=150,
+                                height=150)
+        self.canvas.place(x=0, y=0)
+        self.canvas.create_image(0, 0, image=thumbnail, anchor=tk.NW)
+
+        self.tk_company_box = tk.Entry(width=10)
+        self.tk_company_box.insert(tk.END, "会社名")
+        self.tk_company_box.pack()
+
+        self.tk_date_box = tk.Entry(width=10)
+        self.tk_date_box.insert(tk.END, f"日付")
+        self.tk_date_box.pack()
+
+        self.tk_name_box = tk.Entry(width=10)
+        self.tk_name_box.insert(tk.END, "名前")
+        self.tk_name_box.pack()
+
+        Button = tk.Button(text='update', width=8)
+        Button.bind("<ButtonPress>", self.button_event)
+        Button.pack()
+
+        Button = tk.Button(text='save', width=8)
+        Button.bind("<ButtonPress>", self.button_event)
+        Button.pack()
+
+        self.root.mainloop()
--- e_hanco/hanco.py
+++ e_hanco/hanco.py
@@ -6,6 +6,7 @@ DT_NOW = datetime.now()
 COLOR_WHITE = (255, 255, 255)
 COLOR_RED = (255, 0, 0)
 
+
 def drawing(
         font_type: str,
         img_size: tuple = (300, 300),
@@ -24,13 +25,15 @@ def drawing(
     draw.line((10, 100, 290, 100), fill=COLOR_RED, width=10)
     draw.line((10, 200, 290, 200), fill=COLOR_RED, width=10)
 
-    # テキストを入力
+    # 会社のテキスト入力
     corp_font = ImageFont.truetype(font_type, 28)
     w, h = draw.textsize(company_name, corp_font)
     width = hanco_img.size[0]
     height = hanco_img.size[1]
-    draw.text((((width - w)/2), 50), company_name, font=corp_font, fill=COLOR_RED)
+    draw.text((((width - w)/2), 50), company_name,
+              font=corp_font, fill=COLOR_RED)
 
+    # 日付のテキスト入力
     date_font = ImageFont.truetype(font_type, 60)
     w, h = draw.textsize(seal_date, date_font)
     width = hanco_img.size[0]
@@ -38,11 +41,13 @@ def drawing(
     draw.text((((width - w)/2), ((height - h)/2)),
               seal_date, font=date_font, fill=COLOR_RED)
 
+    # 名前のテキスト入力
     name_font = ImageFont.truetype(font_type, 48)
     w, h = draw.textsize(seal_name, name_font)
     width = hanco_img.size[0]
     height = hanco_img.size[1]
-    draw.text((((width - w)/2), 200), seal_name, font=name_font, fill=COLOR_RED)
+    draw.text((((width - w)/2), 200), seal_name,
+              font=name_font, fill=COLOR_RED)
 
     #  ---------- ---------- 透過処理 ---------- ----------
     trans = Image.new('RGBA', hanco_img.size, (0, 0, 0, 0))
@@ -59,7 +64,9 @@ def drawing(
 
     return trans
 
+
 if __name__ == "__main__":
+    # Debug
     pil_img = drawing("/usr/share/fonts/opentype/noto/NotoSansCJK-Bold.ttc")
     print(pil_img)
     pil_img.show()
